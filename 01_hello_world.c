bc37d5809ed64b0f52d3871bfbc3c1684143329f
Leon Silva <l30silva.o@gmail.com>
2023-01-22 02:29:40 +0000

    feat: describe the main method and stdio header

---
+++ 01_hello_world.c
@@ -0,0 +1,17 @@
+/*
+ * All headers are precompiled and reviewd code avialable and ready to use
+ * Import the standard input-ouput header
+ */
+#include <stdio.h>
+
+/*
+ * C programs start in the main method
+ * main can return any data type, but the operating system recognizes the
+ * int as an indication of error numbers
+ */
+int main() {
+    // printf: print format
+    printf("Hello World!\n");
+
+    return 0;
+}
